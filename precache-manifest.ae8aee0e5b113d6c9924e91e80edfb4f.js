26b448c813d1909c1f7deb748b5146ccb807c307
Jester Merida <69309561+ifortressjess@users.noreply.github.com>
2020-08-07 13:12:03 +0800

    Updates

---
+++ precache-manifest.ae8aee0e5b113d6c9924e91e80edfb4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119eaa3a4d8e4aa040efb2782f9b41f4",
+    "url": "/test-web/index.html"
+  },
+  {
+    "revision": "7a36c6adbbdf97f3a258",
+    "url": "/test-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d450a65a2ea0ab6d358",
+    "url": "/test-web/static/js/2.e5f0e2ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-web/static/js/2.e5f0e2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a36c6adbbdf97f3a258",
+    "url": "/test-web/static/js/main.bfec8e63.chunk.js"
+  },
+  {
+    "revision": "b94cb52a64663ae1b8c9",
+    "url": "/test-web/static/js/runtime-main.2c6a5b84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
